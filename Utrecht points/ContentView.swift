7f739c081762740900837366978aa1671424e7ff
Kirill Ki <kirillkalapov@gmail.com>
2022-11-07 18:56:54 +0100

    Initial Commit

---
+++ Utrecht points/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Utrecht points
+//
+//  Created by  Mr.Ki on 07.11.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Utrecht points/Utrecht_pointsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Utrecht_pointsApp.swift
+//  Utrecht points
+//
+//  Created by  Mr.Ki on 07.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct Utrecht_pointsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
